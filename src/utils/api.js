c31120e869a0fbf8d189d39e4e8540adbbf56e88
Javiei <Herrerajaviersoto@gmail.com>
2025-08-16 21:32:12 -0400

    Actualizar rutas: simplificar a solo Haina, Santo Domingo y Salcedo

---
+++ src/utils/api.js
@@ -0,0 +1,8 @@
+  // Rutas (por ahora las mantenemos como constantes, pero podrían venir de la BD)
+  static getRutas() {
+    return [
+      'Haina',
+      'Santo Domingo',
+      'Salcedo'
+    ];
+  }
